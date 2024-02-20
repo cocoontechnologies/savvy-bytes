85d9556afc5a5c9a1d75d2fb66ce75f1322aac88
Miguel <miguelarciagaa@gmail.com>
2024-02-21 02:34:57 +0800

    slider na maangas

---
+++ src/data/projects.js
@@ -0,0 +1,40 @@
+export const projects = [
+    {
+        id: 'rabbies',
+        title: 'Rabbies Cosmetics',
+        label: 'Rabbies Cosmetics',
+        tags: ['shopify'],
+        description: "This design showcases the user journey within Company X's ride-hailing app, simplifying your understanding of the entire process. Dive into an effortless flow, starting with a smooth login, followed by a clear and concise onboarding process that guides you on app navigation and functionality. Discover how to effortlessly.",
+        // images: [
+        //     'test-1.s',
+        //     'test-2.s',
+        //     'test-1.s',
+        // ]
+    },
+
+    {
+        id: 'bmsys',
+        title: 'Barangay Management System',
+        label: 'Barangay Management System',
+        tags: ['laravel'],
+        description: "This design showcases the user journey within Company X's ride-hailing app, simplifying your understanding of the entire process. Dive into an effortless flow, starting with a smooth login, followed by a clear and concise onboarding process that guides you on app navigation and functionality. Discover how to effortlessly.",
+        // images: [
+        //     'test-1.s',
+        //     'test-2.s',
+        //     'test-1.s',
+        // ]
+    },
+
+    {
+        id: 'dmrs',
+        title: 'Dan Murdoch Risk Services',
+        label: 'DMRS',
+        tags: ['react.js'],
+        description: "This design showcases the user journey within Company X's ride-hailing app, simplifying your understanding of the entire process. Dive into an effortless flow, starting with a smooth login, followed by a clear and concise onboarding process that guides you on app navigation and functionality. Discover how to effortlessly.",
+        // images: [
+        //     'test-1.s',
+        //     'test-2.s',
+        //     'test-1.s',
+        // ]
+    },
+]
