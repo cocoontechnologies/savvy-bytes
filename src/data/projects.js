efc54ea9904abebffa8b9041e80274f1035e0fbb
Miguel <miguelarciagaa@gmail.com>
2024-02-27 20:56:26 +0800

    responsiveness

--- src/data/projects.js
+++ src/data/projects.js
@@ -5,11 +5,11 @@ export const projects = [
         label: 'Rabbies Cosmetics',
         tags: ['shopify'],
         description: "This design showcases the user journey within Company X's ride-hailing app, simplifying your understanding of the entire process. Dive into an effortless flow, starting with a smooth login, followed by a clear and concise onboarding process that guides you on app navigation and functionality. Discover how to effortlessly.",
-        // images: [
-        //     'test-1.s',
-        //     'test-2.s',
-        //     'test-1.s',
-        // ]
+        images: [
+            'test-1.s',
+            'test-2.s',
+            'test-1.s',
+        ]
     },
 
     {
@@ -18,11 +18,11 @@ export const projects = [
         label: 'Barangay Management System',
         tags: ['laravel'],
         description: "This design showcases the user journey within Company X's ride-hailing app, simplifying your understanding of the entire process. Dive into an effortless flow, starting with a smooth login, followed by a clear and concise onboarding process that guides you on app navigation and functionality. Discover how to effortlessly.",
-        // images: [
-        //     'test-1.s',
-        //     'test-2.s',
-        //     'test-1.s',
-        // ]
+        images: [
+            'test-1.s',
+            'test-2.s',
+            'test-1.s',
+        ]
     },
 
     {
@@ -31,10 +31,10 @@ export const projects = [
         label: 'DMRS',
         tags: ['react.js'],
         description: "This design showcases the user journey within Company X's ride-hailing app, simplifying your understanding of the entire process. Dive into an effortless flow, starting with a smooth login, followed by a clear and concise onboarding process that guides you on app navigation and functionality. Discover how to effortlessly.",
-        // images: [
-        //     'test-1.s',
-        //     'test-2.s',
-        //     'test-1.s',
-        // ]
+        images: [
+            'test-1.s',
+            'test-2.s',
+            'test-1.s',
+        ]
     },
 ]
